b9d59c8f5bebcedf7045fd08565d68bde14ad2bc
Mrgl1203 <gl152217@163.com>
2019-03-14 17:53:41 +0800

    添加权限

--- app/src/main/java/com/gulei/gldemo/MainActivity.java
+++ app/src/main/java/com/gulei/gldemo/MainActivity.java
@@ -1,13 +1,19 @@
 package com.gulei.gldemo;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.gulei.gldemo.activity.BitmapActivity;
 import com.gulei.gldemo.activity.SGLActivity;
 import com.gulei.gldemo.activity.ShapeActivity;
+import com.tbruyelle.rxpermissions2.Permission;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.functions.Consumer;
 
 //参考博客：https://blog.csdn.net/qq_32175491/article/details/79091647
 //https://blog.csdn.net/junzia/article/details/52820177
@@ -17,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.requestEach(Manifest.permission.CAMERA
+                , Manifest.permission.RECORD_AUDIO
+                , Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Permission>() {
+                    @Override
+                    public void accept(Permission permission) throws Exception {
+                        if (permission.granted) {
+
+                        } else if (permission.shouldShowRequestPermissionRationale) {
+                            Toast.makeText(MainActivity.this, "不给" + permission.name + "不让玩哦", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(MainActivity.this, "哼，不和你玩了", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                });
     }
 
     public void drawShape(View view) {
